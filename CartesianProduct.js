3ab5a924492a8d01654f324afe082c08520838e5
The13ms <107684060+The13ms@users.noreply.github.com>
2022-10-22 12:52:10 +0200

    Add files via upload

---
+++ CartesianProduct.js
@@ -0,0 +1,10 @@
+function cartesianProduct(arr1, arr2) {
+    let result = []
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            result.push([arr1[i], arr2[j]])
+        }
+    } return result
+}
+//Big-O = O(mn)
+console.log(cartesianProduct([1, 2], [3, 4, 5]))
